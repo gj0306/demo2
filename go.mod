d3d1b578cf46a0bb1e750664cf5758967ee8f96d
BOSS </>
2020-11-17 17:21:54 +0800

    [add]信号量

--- go.mod
+++ go.mod
@@ -6,8 +6,10 @@ require (
 	github.com/golang/protobuf v1.4.3
 	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2
 	github.com/levigross/grequests v0.0.0-20190908174114-253788527a1a // indirect
+	github.com/marusama/semaphore/v2 v2.4.1
 	github.com/qiniu/api.v7 v7.2.5+incompatible
 	github.com/zwczou/jpush v0.0.0-20180527005611-a5e77e351698
+	go.uber.org/atomic v1.4.0
 	go.uber.org/zap v1.10.0
 	golang.org/x/net v0.0.0-20201031054903-ff519b6c9102
 	google.golang.org/grpc v1.33.1
---
+++ utils/semaphore/pv_demo_test.go
@@ -0,0 +1,32 @@
+package semaphore
+
+import (
+	"context"
+	"github.com/marusama/semaphore/v2"
+	"testing"
+	"time"
+)
+
+func TestPvDome(t *testing.T)  {
+	//新建一个5的信号量
+	sem := semaphore.New(5)
+	ctx := context.Background()
+
+	/* 获取信号量 */
+
+	//用上下文获取n
+	_ = sem.Acquire(ctx, 1)
+	//无阻塞信号量
+	_ = sem.TryAcquire(1)
+	//超时信号量
+	ctx1,_ := context.WithTimeout(context.Background(), time.Second)
+	_ = sem.Acquire(ctx1, 1)
+
+	/* 释放信号量 */
+	sem.Release(1)
+
+
+	/* 修改信号量数量 */
+	sem.SetLimit(6)
+
+}
